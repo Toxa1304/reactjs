888c37a1a7f3de775f23c1aa9443d389abf416d9
Toxa1304 <55582541+Toxa1304@users.noreply.github.com>
2022-05-24 19:57:24 +0300

    able to get api data, mockup table runnung

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';  
+import ReactTable from "react-table-6";  
+import "react-table-6/react-table.css" 
+  
+class App extends Component {  
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ 
 
-export default App;
+
+  render() {  
+    let planetData;
+    let url = 'https://swapi.dev/api/planets/2';
+    let name, RotationPeriod, OrbitalPeriod, diameter, climate, gravity, terrain, surfaceWater, population;
+    fetch(url)
+      .then(response => response.json())
+      .then(json =>{
+        console.log(json.name);
+        console.log(json.population)
+
+      }) 
+      
+
+    
+    //console.log(json. );
+ 
+     const data1 = [{  
+        name: 'Ayaan',  
+        age: 26,
+        sex: 'male'  
+        },{  
+         name: 'Ahana',  
+         age: 22,  
+         sex: 'male'  
+         },{  
+         name: 'Peter',  
+         age: 40 ,
+         sex: 'male'       
+         },{  
+         name: 'Virat',  
+         age: 30  ,
+         sex: 'male'  
+         },{  
+         name: 'Rohit',  
+         age: 32  ,
+         sex: 'male'  
+         },{   
+         name: 'Dhoni',  
+         age: 37  ,
+         sex: 'male'  
+         }]  
+     const columns = [{  
+       Header: 'Name',  
+       accessor: 'name'  
+       },{  
+       Header: 'Age',  
+       accessor: 'age'  
+       },{  
+        Header: 'Sex',  
+        accessor: 'sex'  
+        }]   
+    return (  
+          
+          <div>  
+              <ReactTable  
+                  data={data1}  
+                  columns={columns}  
+                  defaultPageSize = {6}  
+                  pageSizeOptions = {[2,4, 6]}  
+                 
+              />  
+          </div>        
+    )  
+  }  
+}  
+export default App;  
